1a6efc655d2fdd4083842c9d361bd9522cf017ad
unknown <juliusahianu@gmail.com>
2019-06-18 18:04:15 -0700

    added

---
+++ Myclass2.java
@@ -0,0 +1,20 @@
+class Myclass2{
+
+ int x = 5;
+
+
+ public static void main(String[] args){
+ 
+ Myclass2 myObj1 = new Myclass2();
+ Myclass2 myObj2 = new Myclass2(); 
+
+	System.out.println(myObj1.x);
+	System.out.println(myObj2.x);
+
+ }
+
+
+
+
+
+}
